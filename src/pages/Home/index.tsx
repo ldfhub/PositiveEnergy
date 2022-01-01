afd6b8897bb8b8abb0980acf13dfc28a0da77cb3
lidengfeng <coderldf580@163.com>
2022-01-01 21:57:46 +0800

    ci: 测试微信登陆

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,26 @@
 import React, { FC, } from 'react';
-import { View } from '@tarojs/components';
+import Taro from '@tarojs/taro';
+import { View, OpenData } from '@tarojs/components';
+import { AtButton, AtModal, AtModalHeader, AtModalContent, AtModalAction } from 'taro-ui';
 import { IHomeProps } from './index.interface';
 import TabBar from '../../components/TabBar/index'
 import Joke from '../../components/Joke/index'
 
 const Home:FC<IHomeProps> = () => {
+  const getAvatar = (e) => [
+    Taro.getUserProfile({
+      desc: '千零粉丝获取', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      lang: 'zh_CN',
+      success: (res) => {
+
+      }
+    })
+  ]
   return (
     <View>
       这是首页
-      <Joke></Joke>
+      {/* <Joke></Joke> */}
+      <AtButton onClick={getAvatar}>微信一键登录</AtButton>
     </View>
   );
 }
