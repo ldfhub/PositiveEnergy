745c3b1c6f67e8095ffbb9a26781f8c4f885561d
lidengfeng <coderldf580@163.com>
2022-02-20 22:10:54 +0800

    ci: 添加文字复制

--- src/pages/Home/component/customTable/index.tsx
+++ src/pages/Home/component/customTable/index.tsx
@@ -1,22 +1,38 @@
 import { View, Text } from '@tarojs/components';
-import React, { FC, ReactElement} from 'react';
+import React, { FC, ReactElement, useState} from 'react';
+import copy from "copy-to-clipboard";
+import { AtToast } from "taro-ui"
+import Taro from '@tarojs/taro';
 import styles from './index.module.scss';
+
 interface IProps {
-  info: {content: string; id: string; type: string;}
+  info: {content: string; id: string; type: string; collect: string; easyLike:string;}
 }
 
 const CustomTable: FC<IProps> = (props):ReactElement => {
   const { info } = props;
+  const copyText = (textToCopy) => {
+    return () => {
+      Taro.setClipboardData({
+        data: textToCopy,
+        success: function (res) {
+          console.log('复制成功', res);
+        }
+      })
+    }
+  }
   return (
     <View className={styles.customTable}>
       <View className={styles.thumbnail}></View>
       <View className={styles.contentInfo}>
         <Text className={styles.textContent}>
-          {info.content}
+          {info?.content}
         </Text>
         <View className={styles.btnInfo}>
-          <View className='at-icon at-icon-heart'></View>
-          <View className='at-icon at-icon-star'></View>
+          <View className='at-icon at-icon-heart'>{(Number(info.easyLike))}</View>
+          <View className='at-icon at-icon-star'>{(Number(info.collect))}</View>
+          <Text onClick={copyText(info.content)}>复制文字</Text>
+          <Text>详情</Text>
         </View>
       </View>
     </View>
